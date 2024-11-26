41dc8872ad3870899451c9883ffceaa250917352
laaksoharju <mikael.laaksoharju@it.uu.se>
2024-11-26 23:41:50 +0100

    first version of skeleton code

---
+++ server/Data.js
@@ -0,0 +1,130 @@
+'use strict';
+import {readFileSync} from "fs";
+
+// Store data in an object to keep the global namespace clean. In an actual implementation this would be interfacing a database...
+function Data() {
+  this.polls = {};
+  this.polls['test'] = {
+    lang: "en",
+    questions: [
+      {q: "How old are you?", 
+       a: ["0-13", "14-18", "19-25", "26-35", "36-45","45-"]
+      },
+      {q: "How much do you enjoy coding?", 
+       a: ["1", "2", "3", "4", "5"]
+      }
+    ],
+    answers: [],
+    currentQuestion: 0,
+    participants: []
+  }
+}
+
+/***********************************************
+For performance reasons, methods are added to the
+prototype of the Data object/class
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Closures
+***********************************************/
+
+Data.prototype.pollExists = function (pollId) {
+  return typeof this.polls[pollId] !== "undefined"
+}
+
+Data.prototype.getUILabels = function (lang) {
+  //check if lang is valid before trying to load the dictionary file
+  if (!["en", "sv"].some( el => el === lang))
+    lang = "en";
+  const labels = readFileSync("./server/data/labels-" + lang + ".json");
+  return JSON.parse(labels);
+}
+
+Data.prototype.createPoll = function(pollId, lang="en") {
+  if (!this.pollExists(pollId)) {
+    let poll = {};
+    poll.lang = lang;  
+    poll.questions = [];
+    poll.answers = [];
+    poll.participants = [];
+    poll.currentQuestion = 0;              
+    this.polls[pollId] = poll;
+    console.log("poll created", pollId, poll);
+  }
+  return this.polls[pollId];
+}
+
+Data.prototype.getPoll = function(pollId) {
+  if (this.pollExists(pollId)) {
+    return this.polls[pollId];
+  }
+  return {};
+}
+
+Data.prototype.participateInPoll = function(pollId, name) {
+  console.log("participant will be added to", pollId, name);
+  if (this.pollExists(pollId)) {
+    this.polls[pollId].participants.push({name: name, answers: []})
+  }
+}
+
+Data.prototype.getParticipants = function(pollId) {
+  const poll = this.polls[pollId];
+  console.log("participants requested for", pollId);
+  if (this.pollExists(pollId)) { 
+    return this.polls[pollId].participants
+  }
+  return [];
+}
+
+Data.prototype.addQuestion = function(pollId, q) {
+  if (this.pollExists(pollId)) {
+    this.polls[pollId].questions.push(q);
+  }
+}
+
+Data.prototype.getQuestion = function(pollId, qId = null) {
+  if (this.pollExists(pollId)) {
+    const poll = this.polls[pollId];
+    if (qId !== null) {
+      poll.currentQuestion = qId;
+    }
+    return poll.questions[poll.currentQuestion];
+  }
+  return {}
+}
+
+Data.prototype.getSubmittedAnswers = function(pollId) {
+  if (this.pollExists(pollId)) {
+    const poll = this.polls[pollId];
+    const answers = poll.answers[poll.currentQuestion];
+    if (typeof poll.questions[poll.currentQuestion] !== 'undefined') {
+      return answers;
+    }
+  }
+  return {}
+}
+
+Data.prototype.submitAnswer = function(pollId, answer) {
+  if (this.pollExists(pollId)) {
+    const poll = this.polls[pollId];
+    let answers = poll.answers[poll.currentQuestion];
+    // create answers object if no answers have yet been submitted
+    if (typeof answers !== 'object') {
+      answers = {};
+      answers[answer] = 1;
+      poll.answers.push(answers);
+    }
+    // create answer property if that specific answer has not yet been submitted
+    else if (typeof answers[answer] === 'undefined') {
+      answers[answer] = 1;
+    }
+    // if the property already exists, increase the number
+    else
+      answers[answer] += 1
+    console.log("answers looks like ", answers, typeof answers);
+  }
+}
+
+export { Data };
+
+
+
---
+++ server/sockets.js
@@ -0,0 +1,42 @@
+function sockets(io, socket, data) {
+  
+  socket.on('getUILabels', function(lang) {
+    socket.emit('uiLabels', data.getUILabels(lang));
+  });
+
+  socket.on('createPoll', function(d) {
+    data.createPoll(d.pollId, d.lang)
+    socket.emit('pollData', data.getPoll(d.pollId));
+  });
+
+  socket.on('addQuestion', function(d) {
+    data.addQuestion(d.pollId, {q: d.q, a: d.a});
+    socket.emit('questionUpdate', data.getQuestion(d.pollId));
+  });
+
+  socket.on('joinPoll', function(pollId) {
+    socket.join(pollId);
+    socket.emit('questionUpdate', data.getQuestion(pollId))
+    socket.emit('submittedAnswersUpdate', data.getSubmittedAnswers(pollId));
+  });
+
+  socket.on('participateInPoll', function(d) {
+    data.participateInPoll(d.pollId, d.name);
+    io.to(d.pollId).emit('participantsUpdate', data.getParticipants(d.pollId));
+  });
+  socket.on('startPoll', function(pollId) {
+    io.to(pollId).emit('startPoll');
+  })
+  socket.on('runQuestion', function(d) {
+    let question = data.getQuestion(d.pollId, d.questionNumber);
+    io.to(d.pollId).emit('questionUpdate', question);
+    io.to(d.pollId).emit('submittedAnswersUpdate', data.getSubmittedAnswers(d.pollId));
+  });
+
+  socket.on('submitAnswer', function(d) {
+    data.submitAnswer(d.pollId, d.answer);
+    io.to(d.pollId).emit('submittedAnswersUpdate', data.getSubmittedAnswers(d.pollId));
+  }); 
+}
+
+export { sockets };
