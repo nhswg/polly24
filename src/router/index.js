50317f6245808c13ef946655fb87835e286ce76f
Henrik Swanberg <hsw@HS-MacBook-Air.local>
2024-12-06 17:08:53 +0100

    Name change of LobbyView

--- src/router/index.js
+++ src/router/index.js
@@ -17,7 +17,7 @@ const router = createRouter({
     {
       path: '/playerlobby/',
       name: 'PlayerLobby',
-      component: () => import('../views/LobbyView.vue')
+      component: () => import('../views/JoinGameView.vue')
     },
 
     {
