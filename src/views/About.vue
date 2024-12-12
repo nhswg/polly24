0a6abbd0217c9bc2d3c2c9d6a473a83cacc49f67
Henrik Swanberg <hsw@student-215-64.eduroam.uu.se>
2024-12-12 16:01:42 +0100

    css changes about.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,79 +1,97 @@
 <template>
-
-    <router-link to="/">
-        <button class="header-button">
-            {{uiLabels.backButton}}
+  <div class="sketchdle-container">
+    <header>
+      <router-link to="/">
+        <button class="title-button">
+          ←
         </button>
-    </router-link>
+      </router-link>
 
-    <div class="content">
-      <h1 class="title">{{ uiLabels.gameRules }}</h1>
-  
-      <p class="text">
-        {{ uiLabels.aboutPart1 }}
-      </p>
-  
-      <p class="text">
-        {{ uiLabels.aboutPart2 }}
-      </p>
-  
-      <p class="text">
-        {{ uiLabels.aboutPart3 }}
-      </p>
-  
-      <p class="text">
-        {{ uiLabels.aboutPart4 }}
-      </p>
-    </div>
-  </template>
-  
-  <script>
-  import io from 'socket.io-client';
-  const socket = io("localhost:3000");
+      <h1 class="title">Sketchdle</h1>
 
-  export default {
+      <button class="flag-container" v-on:click="switchLanguage">
+        <img src="/img/flag.png" alt="changeLanguagePic">
+      </button>
+    </header>
+    <div class="header-line"></div>
+
+    <main>
+      <div class="content">
+        <h1 class="title">{{ uiLabels.gameRules }}</h1>
+
+        <p class="text">
+          {{ uiLabels.aboutPart1 }}
+        </p>
+  
+        <p class="text">
+          {{ uiLabels.aboutPart2 }}
+        </p>
+  
+        <p class="text">
+          {{ uiLabels.aboutPart3 }}
+        </p>
+  
+        <p class="text">
+          {{ uiLabels.aboutPart4 }}
+        </p>
+      </div>
+    </main>
+  </div>
+</template>
+  
+<script>
+import io from 'socket.io-client';
+const socket = io("localhost:3000");
+
+export default {
   name: 'About',
-  data: function () {
+  data() {
     return {
       uiLabels: {},
       lang: localStorage.getItem( "lang") || "en",
     }
   },
-  created: function () {
+  created() {
     socket.on( "uiLabels", labels => this.uiLabels = labels );
     socket.emit( "getUILabels", this.lang );
   },
 }
+</script>
+  
+<style scoped>
 
-  </script>
-  
-  <style>
-  .content {
-    max-width: 800px; /* Sätt önskad maximal bredd */
-    margin: 0 auto; /* Centrera innehållet horisontellt */
-    padding: 20px; /* Valfri padding för att ge lite utrymme */
-  }
-  
-  .title {
-    text-align: center;
-    margin-bottom: 20px;
-  }
-  
-  .text {
-    font-size: 20px;
-    text-align: justify; /* Eller 'left' om du föredrar vänsterjusterad text */
-    margin-bottom: 20px;
-  }
-
-  .header-button {
-    font-size: 1.5rem;
-    border: 0.2em solid black;
-    width: auto;
-    height: 50px;
-    position: absolute; 
-    display: inline-block;
-    top: 120px; 
-    left: 120px; 
+.flag-container {
+    border: 0.1em solid black;
+    border-radius: 0.2em;
+    padding: 0;
+    transition: transform 0.3s ease;
 }
-  </style>
-  
+
+.flag-container img {
+    display: block;
+    width: 80px;
+    height: auto;
+}
+.content {
+  max-width: 800px; /* Sätt önskad maximal bredd */
+  margin: 0 auto; /* Centrera innehållet horisontellt */
+  margin-top: 20px;
+  margin-bottom: 20px;
+  padding: 20px; /* Valfri padding för att ge lite utrymme */
+  background-color: rgb(247, 247, 247);
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
+  border-radius: 1rem;
+}
+.content h1 {
+  font-size: 3rem;
+  margin-top: 0px;
+  margin-bottom: 20px;
+  text-align: left;
+}
+
+.text {
+  font-size: 20px;
+  text-align: justify; /* Eller 'left' om du föredrar vänsterjusterad text */
+  margin-bottom: 20px;
+}
+</style>
